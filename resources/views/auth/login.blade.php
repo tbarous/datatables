12b4cb5b3c845b2e30f93d5f9728956332bfbdcc
Barous Tasos <abarous@qualco.int>
2019-06-04 17:10:21 +0300

    new

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -3,25 +3,26 @@
     @include('auth.navbar')
     <div class="container">
         <div class="row justify-content-center">
+
             <div class="col-md-6">
-                <div class="card mt-5 border-0 pb-4 bg-transparent">
-                    <div class="text-center card-header bg-transparent text-white border-0">
+                <div>
+                    <div style="font-size:30px;" class="text-center text-white">
                         VICTUS
                     </div>
 
-                    <div class="card-body pt-5">
+                    <div class="mt-5">
                         <form method="POST" action="{{ route('login') }}">
                             @csrf
 
-                            <div class="form-group row">
-                                <label for="username" class="col-sm-12 text-white col-form-label">Username</label>
+                            <div class="form-group row justify-content-center">
+                                <label for="username" class="col-sm-12 offset-sm-6 text-white col-form-label">Username</label>
                                 <div class="col-md-6">
                                     <input id="username" type="text" class="form-control" name="username" value="{{ old('username') }}" required autofocus>
                                 </div>
                             </div>
 
-                            <div class="form-group row">
-                                <label for="password" class="col-md-12 text-white col-form-label">Password</label>
+                            <div class="form-group row justify-content-center">
+                                <label for="password" class="col-md-12 offset-sm-6 text-white col-form-label">Password</label>
 
                                 <div class="col-md-6">
                                     <input id="password" type="password" class="form-control" name="password" required autocomplete="current-password">
@@ -29,7 +30,7 @@
                             </div>
 
                             <div class="form-group row mb-0">
-                                <div class="col-md-8 offset-md-4">
+                                <div class="col-md-8 offset-md-3">
                                     @if ($errors->has('username'))
                                         <span class="text-danger" role="alert">
                                             <strong>{{ $errors->first('username') }}</strong>
@@ -38,8 +39,8 @@
                                 </div>
                             </div>
 
-                            <div class="form-group row mb-0 mt-4">
-                                <div class="col-md-6 offset-md-4">
+                            <div class="form-group row mb-0 mt-4 justify-content-center">
+                                <div class="col-md-6">
                                     <button type="submit" class="btn btn-primary w-100">
                                         Login
                                     </button>
