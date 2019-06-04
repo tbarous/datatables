ba57c624b58ff8bf03003f9cb77bf0a7ce40cc3c
Barous Tasos <abarous@qualco.int>
2019-06-04 17:27:54 +0300

    new

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -6,25 +6,25 @@
 
             <div class="col-md-6">
                 <div>
-                    <div style="font-size:30px;" class="text-center text-white">
-                        VICTUS
+                    <div style="font-size:30px;letter-spacing: 5px;" class="text-center text-white">
+                        VICTUS NETWORKS
                     </div>
 
-                    <div class="mt-5">
+                    <div class="mt-5 pb-5">
                         <form method="POST" action="{{ route('login') }}">
                             @csrf
 
                             <div class="form-group row justify-content-center">
-                                <label for="username" class="col-sm-12 offset-sm-6 text-white col-form-label">Username</label>
-                                <div class="col-md-6">
+                                <label for="username" class="col-sm-12 offset-sm-4 text-white col-form-label">Username</label>
+                                <div class="col-md-8">
                                     <input id="username" type="text" class="form-control" name="username" value="{{ old('username') }}" required autofocus>
                                 </div>
                             </div>
 
                             <div class="form-group row justify-content-center">
-                                <label for="password" class="col-md-12 offset-sm-6 text-white col-form-label">Password</label>
+                                <label for="password" class="col-md-12 offset-sm-4 text-white col-form-label">Password</label>
 
-                                <div class="col-md-6">
+                                <div class="col-md-8">
                                     <input id="password" type="password" class="form-control" name="password" required autocomplete="current-password">
                                 </div>
                             </div>
@@ -40,7 +40,7 @@
                             </div>
 
                             <div class="form-group row mb-0 mt-4 justify-content-center">
-                                <div class="col-md-6">
+                                <div class="col-md-8">
                                     <button type="submit" class="btn btn-primary w-100">
                                         Login
                                     </button>
--- resources/views/layouts/auth.blade.php
+++ resources/views/layouts/auth.blade.php
@@ -1,5 +1,5 @@
 <!DOCTYPE html>
-<html lang="en">
+<html lang="en" style="    overflow-y: hidden;">
 <head>
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
@@ -9,10 +9,35 @@
     <link href="{{ asset('css/auth.css') }}" rel="stylesheet">
     <script src="https://cdnjs.cloudflare.com/ajax/libs/vue/2.6.10/vue.min.js"></script>
     <!-- <link href="https://cdnjs.cloudflare.com/ajax/libs/mdbootstrap/4.8.2/css/mdb.min.css" rel="stylesheet"> -->
+	<style>
+		#app{
+		    display: flex;
+		    align-items: center;
+		    height: 100vh;
+		   	font-family: 'Nunito'!important;
+		    background-image: url('/images/tel.jpg');
+		    background-size: cover;
+		    overflow: hidden;
+		    box-shadow: inset 0 0 20px rgba(0,0,0,0.9);
+		}
+
+		.overlay{
+			position: absolute;
+			height: 100%;
+			width: 100%;
+			background: rgba(0,0,0,0.3)
+		}
+
+		input, button{
+		    height: 50px!important;
+	        font-size: 19px!important;
+		}
+	</style>
 </head>
 <body>
 	<div id="app">
-		  @yield('content')
+		<div class="overlay"></div>
+		@yield('content')
 	</div>
   
     <script>
